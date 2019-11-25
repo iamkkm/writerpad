2b576f4b39167ad2d9fb3473adc4247b039541b5
iamkkm <kamalkishormeashwari@gmail.com>
2019-11-25 16:27:38 +0530

    supperssions.xml file added

--- src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
+++ src/main/java/com/xebia/fs101/writerpad/WriterpadApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WriterpadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WriterpadApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WriterpadApplication.class, args);
+    }
 }
